f33f30f3e18677dd24dca6da16355734a65c1223
hdhdhdhhdhdhhh1-byte <hdhdhdhhdhdhhh1@gmail.com>
2026-08-06 17:47:58 +0300

    Add environment example configuration

--- vite.mobile.config.ts
+++ vite.mobile.config.ts
@@ -3,14 +3,21 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  appType: "spa",
+
   plugins: [
     react(),
     tsconfigPaths(),
   ],
 
+  resolve: {
+    conditions: ["browser"],
+  },
+
   build: {
     outDir: ".output/public",
     emptyOutDir: true,
+    ssr: false,
   },
 
   server: {
